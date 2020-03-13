97529628083a750df6e8611894b1792d6b654b6f
Nikita Holovnin <Golovninnikita@ukr.net>
2020-03-13 22:44:39 +0200

    complete react-hw-07

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import contactsSelectors from '../../redux/contacts/contactsSelectors';
+import TextNotification from '../TextNotification';
+import ListItem from './ListItem';
+import slideItemTransition from '../transitions/slideItem.module.css';
+
+const List = styled.ul`
+  margin-bottom: 3rem;
+`;
+
+function ContactList({ contacts, visibleContacts }) {
+  return (
+    <>
+      <TransitionGroup component={List}>
+        {visibleContacts.map(({ id }) => (
+          <CSSTransition
+            key={id}
+            timeout={250}
+            classNames={slideItemTransition}
+          >
+            <ListItem id={id} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+      {contacts.length === 0 && (
+        <TextNotification message={'You have no contacts. Add some :)'} />
+      )}
+      {contacts.length > 1 && visibleContacts.length === 0 && (
+        <TextNotification message={'No contacts found :('} />
+      )}
+    </>
+  );
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.exact({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      }),
+    ),
+    PropTypes.array,
+  ]),
+};
+
+const mapStateToProps = state => {
+  return {
+    contacts: contactsSelectors.getContacts(state),
+    visibleContacts: contactsSelectors.getVisibleContacts(state),
+  };
+};
+
+export default connect(mapStateToProps)(ContactList);
